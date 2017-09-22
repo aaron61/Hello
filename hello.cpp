06235f0ef2614971db147ce41969fd3912d17fd1
aaron61 <32183775+aaron61@users.noreply.github.com>
2017-09-21 21:56:30 -0400

    Hello
    This is a simple Hello world application that operates from
    the conslole. It prompts for the user name and outputs Hello $name

---
+++ hello.cpp
@@ -0,0 +1,21 @@
+/**********************************************
+This is a Hello World application
+designed by: Aaron Marples
+grayeagle161@gmail.com
+**********************************************/
+
+#include <iostream>
+
+int main()
+{
+using std::cin;
+using std::cout;
+using std::endl;
+
+string name;
+
+cout << "Please enter your name: ";
+cin >> name;
+cout << "\nHello, " << name << endl;
+exit 0;
+}
